5010f186661767eab178090b49d02ba644412e54
feriwendi <49948965+feriwendi@users.noreply.github.com>
2019-04-24 21:08:31 +0800

    First Commit
    First Commit

---
+++ hubungi.php
@@ -0,0 +1,5 @@
+<center>
+	<h2>Hubungi saya di</h2><br>
+		<i class="glyphicon glyphicon-envelope"></i> 175463@smktelkommedan.sch.id<br>
+		<i class="glyphicon glyphicon-phone"></i> <a href="">0821984918</a>
+</center>
